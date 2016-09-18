3c9c1fd46f876055f911376d73e92cd3b8ae2009
foy <ljs2342003@gmail.com>
2016-09-18 18:02:58 +0800

    add resources

---
+++ boot-entity-engine-service/src/main/java/com/pactera/pds/entityengine/Application.java
@@ -0,0 +1,18 @@
+package com.pactera.pds.entityengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Foy Lian
+ * Date: 2016-09-18
+ * Time: 17:55
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ boot-entity-engine-service/src/main/java/com/pactera/pds/entityengine/config/EntityEngineConfiguration.java
@@ -0,0 +1,19 @@
+package com.pactera.pds.entityengine.config;
+
+import org.ofbiz.core.entity.GenericDAO;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Foy Lian
+ * Date: 2016-09-18
+ * Time: 17:57
+ */
+@Configuration
+public class EntityEngineConfiguration {
+    @Bean
+    public GenericDAO getProjectDao() {
+        return new GenericDAO("Project");
+    }
+}
