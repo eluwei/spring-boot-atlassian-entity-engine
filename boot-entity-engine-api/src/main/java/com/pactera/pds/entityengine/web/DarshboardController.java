419ed0ada4d1d513a02de034c21f26e813a9333c
foy <ljs2342003@gmail.com>
2016-09-19 17:50:33 +0800

    transaction test..

--- boot-entity-engine-api/src/main/java/com/pactera/pds/entityengine/web/DarshboardController.java
+++ boot-entity-engine-api/src/main/java/com/pactera/pds/entityengine/web/DarshboardController.java
@@ -3,11 +3,11 @@ package com.pactera.pds.entityengine.web;
 import org.ofbiz.core.entity.DelegatorInterface;
 import org.ofbiz.core.entity.GenericEntityException;
 import org.ofbiz.core.entity.GenericValue;
-import org.ofbiz.core.entity.TransactionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -32,26 +32,20 @@ public class DarshboardController {
     //test roll back!
     @RequestMapping("/trans")
     public Object Trans() throws GenericEntityException {
-        try{
-            TransactionUtil.begin();
-            Map obj1=new HashMap<>();
-            obj1.put("id",22);
-            obj1.put("key","22");
-            obj1.put("counter", 22);
-            delegator.create("Project", obj1);
-            int a =0;
-            int b =1/a;
-            Map obj2=new HashMap<>();
-            obj2.put("id",22);
-            obj2.put("key","22");
-            obj2.put("counter", 22);
-            delegator.create("Project", obj2);
-            TransactionUtil.commit();
-        }catch (Exception e){
-            TransactionUtil.rollback();
-
-        }
-
+        Map obj1=new HashMap<>();
+        obj1.put("id",22);
+        obj1.put("key","11");
+        obj1.put("counter", 11);
+        GenericValue g1 = delegator.makeValue("Project", obj1);
+        Map obj2=new HashMap<>();
+        obj2.put("id",22);
+        obj2.put("key","22");
+        obj2.put("counter", 22);
+        GenericValue g2 = delegator.makeValue("Project", obj2);
+        List<GenericValue> savePoint = new ArrayList<>();
+        savePoint.add(g1);
+        savePoint.add(g2);
+        delegator.storeAll(savePoint);//duplicate key, will auto rollback..
         return "aa";
     }
 
